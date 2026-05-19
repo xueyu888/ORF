ffed3816d1dbd4687128a28b770aeda20f77c219
xueyu <xueuy@qq.com>
2026-05-19 21:54:51 +0800

    feat: add Mattermost Jira reminder

--- server/integrations/index.ts
+++ server/integrations/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { registerGitHubMattermostSync } from "./github-mattermost-sync";
+import { registerMattermostJiraReminder } from "./mattermost-jira-reminder";
 
 export function registerOptionalIntegrations(app: FastifyInstance) {
   registerGitHubMattermostSync(app);
+  registerMattermostJiraReminder(app);
 }
