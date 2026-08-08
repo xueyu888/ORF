794273f3363f651a38c6775a5334f2e7759db8f2
xueyu <xueuy@qq.com>
2026-08-08 15:35:41 +0800

    删除反馈 server 入口无人使用的内部类型导出
    按包外实际导入矩阵收窄 @orf/feedback-module/server：删除无人使用的 activity、digest、read model、notification、subscription、write model 和 transfer 类型导出，以及 runFeedbackDailyDigestSweep、publishPendingFeedbackNotificationDispatches 这类无人包外调用的函数导出。
    
    本次不改变运行逻辑，只减少 public/server 可见契约，避免内部类型继续成为包外旧路径。

--- modules/feedback/src/public/server.ts
+++ modules/feedback/src/public/server.ts
@@ -3,20 +3,9 @@ export type FeedbackModuleStop = () => Promise<void> | void;
 export {
   markFeedbackViewed,
   recordFeedbackCommentCreatedActivity,
-  type FeedbackActivityDatabase,
-  type FeedbackCommentCreatedActivityInput,
-  type FeedbackViewedInput,
-  type FeedbackViewedResult,
 } from "../server/activity";
 export {
-  runFeedbackDailyDigestSweep,
   startFeedbackDailyDigestScheduler,
-  type FeedbackDailyDigestConfig,
-  type FeedbackDailyDigestDatabase,
-  type FeedbackDailyDigestLogger,
-  type FeedbackDailyDigestNotificationInput,
-  type FeedbackDailyDigestRecipient,
-  type FeedbackDailyDigestRuntime,
 } from "../server/dailyDigestScheduler";
 export {
   createFeedbackReferenceProvider,
@@ -26,19 +15,10 @@ export {
   listFeedbackReferences,
   resolveFeedbackCommentTarget,
   searchFeedbackReferences,
-  type FeedbackCommentNotificationFacts,
-  type FeedbackCommentTargetReference,
-  type FeedbackReferenceDatabase,
-  type FeedbackReferenceSummary,
 } from "../server/references";
 export {
   getFeedbackReadModelIssue,
   getFeedbackReadModelIssues,
-  type FeedbackReadModelActivityItem,
-  type FeedbackReadModelDatabase,
-  type FeedbackReadModelIssue,
-  type FeedbackReadModelRelation,
-  type FeedbackReadModelReportAttachment,
   type FeedbackReadModelViewer,
 } from "../server/readModel";
 export {
@@ -47,25 +27,15 @@ export {
   insertFeedbackNotificationDispatch,
   mergeFeedbackNotificationDispatchRecipients,
   publishFeedbackNotificationDispatch,
-  publishPendingFeedbackNotificationDispatches,
   startFeedbackNotificationDispatchWorker,
-  type FeedbackNotificationAttentionLevel,
-  type FeedbackNotificationDeliveryClass,
-  type FeedbackNotificationDispatchDatabase,
   type FeedbackNotificationDispatchDraft,
   type FeedbackNotificationDispatchRecipient,
   type FeedbackNotificationPort,
-  type FeedbackNotificationPortResult,
-  type FeedbackNotificationRecipientReason,
 } from "../server/notificationDispatch";
 export {
   feedbackReportAttachmentResponseContentType,
   getFeedbackReportAttachmentContentFacts,
   listFeedbackReportAttachmentObjectRefs,
-  type FeedbackReportAttachmentContentDatabase,
-  type FeedbackReportAttachmentContentDisposition,
-  type FeedbackReportAttachmentContentFacts,
-  type FeedbackReportAttachmentContentFactsOutcome,
   type FeedbackReportAttachmentObjectRef,
 } from "../server/reportAttachmentContent";
 export {
@@ -73,11 +43,7 @@ export {
   getFeedbackOrdinaryNotificationDispatchRecipients,
   getFeedbackSubscriptionMode,
   setFeedbackSubscriptionMode,
-  type ExplicitFeedbackSubscriptionMode,
   type FeedbackNotificationRecipientDirectory,
-  type FeedbackSubscriptionActor,
-  type FeedbackSubscriptionDatabase,
-  type FeedbackSubscriptionResult,
 } from "../server/subscriptions";
 export {
   addFeedbackIssueRelation,
@@ -87,25 +53,10 @@ export {
   transitionFeedbackIssue,
   updateFeedbackIssueAssignee,
   updateFeedbackIssueMetadata,
-  type AddFeedbackRelationWriteInput,
-  type CreateFeedbackIssueWriteInput,
-  type CreateFeedbackIssueWriteResult,
   type FeedbackCommandResult,
-  type FeedbackCreateDraft,
-  type FeedbackCreateReportAttachmentInput,
   type FeedbackTargetTitleSync,
-  type FeedbackTransitionNotificationDispatchContext,
   type FeedbackTransitionNotificationDispatchFactory,
   type FeedbackWriteActor,
-  type FeedbackWriteClient,
-  type FeedbackWriteDatabase,
-  type FeedbackWriteHost,
-  type RemoveFeedbackRelationWriteInput,
-  type TransitionFeedbackIssueWriteInput,
-  type TransitionFeedbackIssueWriteResult,
-  type UpdateFeedbackAssigneeWriteInput,
-  type UpdateFeedbackAssigneeWriteResult,
-  type UpdateFeedbackMetadataWriteInput,
 } from "../server/writeModel";
 export {
   buildFeedbackBackupZip,
@@ -113,18 +64,6 @@ export {
   feedbackBackupZipFileName,
   preflightFeedbackImport,
   type FeedbackBackupAttachmentFile,
-  type FeedbackBackupAttachmentKind,
-  type FeedbackBackupZipInput,
-  type FeedbackImportCommitResult,
-  type FeedbackImportFieldDiff,
-  type FeedbackImportFieldMapping,
-  type FeedbackImportPreflight,
-  type FeedbackImportReferenceIssue,
-  type FeedbackImportReferenceKind,
-  type FeedbackImportReferenceMappings,
-  type FeedbackImportResultReport,
-  type FeedbackImportSourceKind,
-  type FeedbackImportUpdateDiff,
 } from "../server/transfer";
 
 export interface FeedbackServerHost {
